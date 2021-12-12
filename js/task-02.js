d8c50bc742da00c67f511f5a3140d11bb230ea35
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2021-12-12 08:23:12 +0200

    hw6-task2-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const addList = document.querySelector('ul');
+
+const makeList = array => {
+  return ingredients.map(array => {
+    const elLi = document.createElement('li');
+    elLi.classList.add('item');
+    elLi.textContent = array;
+    return elLi;
+  });
+};
+
+const list = makeList(ingredients);
+console.log(list)
+
+addList.append(...list)
