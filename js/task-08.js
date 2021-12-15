87df53d0d4752f2d8eb6af98a3eeb9a3fad78258
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2021-12-15 19:36:19 +0200

    task8-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  // console.dir(formElements);
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  // console.log("~ pas", password)
+  if (password == false || mail == false) {
+    alert("Заполните все поля");
+    return;
+  }
+
+  const formData = {
+    mail,
+    password,
+  }
+  console.log(formData);
+  form.reset();
+  
+  // const formData = new FormData(event.currentTarget);
+  // formData.forEach((value, name) => {
+  //   console.log("~ name", name)
+  //   console.log("~ value", value)
+    
+  // })
+}
