30640d7b74916022351cf64739b39ab16149a4e5
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2021-12-17 19:03:50 +0200

    change task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,34 @@
-const totalLiEl = document.querySelectorAll('.item').length;
-console.log(`Number of categories: ${totalLiEl}`);
-console.log("")
+const refLi= document.querySelectorAll('.item');   
+console.log(`Number of categories: ${refLi.length}`);
+console.log("");
 
+// console.log(refLi);
 
-const liEl = document.querySelectorAll('.item');
-// console.log(liEl);
-for (const el of liEl) {
-  console.log(`Category: ${el.querySelector('h2').textContent}`);
-  const totalElInCategory = el.querySelectorAll('li').length;
-  console.log(`Elements: ${totalElInCategory}`);
+[...refLi].map(item => { 
+  
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
   console.log("");
-};
+});
 
+
+
+
+
+// const liEl = document.querySelectorAll('.item');
+// console.log(liEl);
+// for (const el of liEl) {
+//   console.log(`Category: ${el.querySelector('h2').textContent}`);
+//   const totalElInCategory = el.querySelectorAll('li').length;
+//   console.log(`Elements: ${totalElInCategory}`);
+//   console.log("");
+// };
+// [...liEl].map(item => {
+//   const category = item.querySelector('h2').textContent;
+//   console.log(`Category: ${category}`);
+//   // console.log(`Elements: ${totalElInCategory}`);
+//   // console.log("");
+// });
 
 
 
