5599eb5f84f529d11653411e9e504c3886a98497
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-03-10 16:57:47 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -91,3 +91,33 @@ console.log(refLi);
 
 // atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
 // atTheOldToad.getPotions();
+
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return makePizza(pizzaName);
+    }
+
+    return onOrderError(
+      `There is no pizza with a name ${pizzaName} in the assortment.`
+    );
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+pizzaPalace.order("Four meats", makePizza, onOrderError);
+pizzaPalace.order("Big Mike", makePizza, onOrderError);
+pizzaPalace.order("Vienna", makePizza, onOrderError);
